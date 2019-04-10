8daf7b7a620000c5f8c626f08d3f0542314799d2
liyang268 <49478258+liyang268@users.noreply.github.com>
2019-04-10 17:46:55 +0800

    Add files via upload

---
+++ springmvcfirst1208/src/cn/itcast/ssm/controller/ItemsController3.java
@@ -0,0 +1,44 @@
+package cn.itcast.ssm.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import cn.itcast.ssm.po.Items;
+
+@Controller
+public class ItemsController3{
+	@RequestMapping("/queryItems")
+	public ModelAndView queryItems() throws Exception{
+		
+		//商品列表
+		List<Items> itemsList = new ArrayList<Items>();
+		
+		Items items_1 = new Items();
+		items_1.setName("联想笔记本");
+		items_1.setPrice(6000f);
+		items_1.setDetail("ThinkPad T430 联想笔记本电脑！");
+		
+		Items items_2 = new Items();
+		items_2.setName("苹果手机");
+		items_2.setPrice(5000f);
+		items_2.setDetail("iphone6苹果手机！");
+		
+		itemsList.add(items_1);
+		itemsList.add(items_2);
+		
+		//创建modelAndView准备填充数据、设置视图
+		ModelAndView modelAndView = new ModelAndView();
+		
+		//填充数据
+		modelAndView.addObject("itemsList", itemsList);
+		//视图
+		modelAndView.setViewName("/WEB-INF/jsp/items/itemsList.jsp");
+		
+		return modelAndView;
+		
+	}
+}
